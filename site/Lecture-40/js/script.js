ed4eccda3ec7e77dffa0dd0f2a243d57f4aec4d1
unknown <mahendrakar987@gmail.com>
2020-08-23 11:27:26 -0400

    23.08 update

--- site/Lecture-40/js/script.js
+++ site/Lecture-40/js/script.js
@@ -126,10 +126,114 @@ result=doOperationOn(100,doubleAll);
 console.log(result)*/
 
 
-function test() {
-	console.log("Hello");
+/*function test() {
+	console.log(this)
+	//console.log("Hello");
+	this.myName="sagar";
+
 }
 test();
+console.log(window.myName)*/
+//function constractr
+/*function Circle (radius) {
+	this.radius =radius;*/
+/*	this.getArea = 
+	   function () {
+	   	return Math.PI * Math.pow(this.radius, 2)
+	   };*/
+	   //This is we can do in simple way that
+/*}
+Circle.prototype.getArea =
+function() {
+	return Math.PI * Math.pow(this.radius, 2)
+};
+var myCircle= new Circle(10);
+console.log(myCircle.getArea())
+*/
+/*function Dog (name) {
+	this.name= name;
+	var self=this;
+}
+Dog.prototype.bark = function () {
+	console.log(self.name + "like barking")
+}
+
+var max=new Dog("Max", "Buddy")
+max.bark();
+*/
+
+//================================================
+//****************Arrays**************
+/*var array=new Array();
+array[0] = "sagar";
+array[1]= 2;
+array[2]= function (name) {
+	console.log("hello "+name)
+};
+array[3] ={course:  "HTML,CSS & JS"};
+
+console.log(array);
+array[2][array[0]];
+console.log(array[3].course)*/
+
+//short hand array creations
+/*var names = [
+{ name : "sagar" } ,
+{ name : "mahendrakar" },
+"kumar"];
+console.log(names);*/
+//agin short
+/*var names =["sagar","kumar" ,"mahendrakar"]
+
+for (var i=0; i< names.length; i++){
+	console.log("Hello "+names[i]);
+}
+*/
+//agin short form is 
+/*var names =["sagar","kumar" ,"mahendrakar"]
+for (var name in names){
+	console.log("Hello "+ names[name]);
+}*/
+
+/*var counter=0;
+var myArray=["sagar", 2, {handle: "sagar mahendrakar"}];
+for (var i = 0; i < myArray.length; i++) {
+	counter++;
+}
+console.log(counter);*/
+//=============================
+//*************clousers**************
+/*function make(multiplay){
+function b() {
+console.log ("multiplay "+ multiplay);
+}
+b();
+
+return (
+function (x){
+return multiplay * x;
+}
+);
+}
+var doubleAll =make(2);
+console.log(doubleAll(10));*/
+
+/*(function(window){
+	var obj ={};
+	obj.dreamOn =function () {
+		console.log("i want to see the global scope")
+	};
+	window.doer =obj;
+})(window);
+doer.dreamOn();*/
+
+
+
+
+
+
+
+
 
 
 
